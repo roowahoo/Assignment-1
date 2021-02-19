e2dabdb48ae449a5b964541065e134f779b52fc4
roowahoo <tohyinru95@gmail.com>
2021-02-19 06:32:28 +0000

    changed setview on map

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
-let singapore = [1.808445, 103.889861];
-let map = L.map('sg-map').setView(singapore, 8);
+let singapore = [28.147087, 64.909175];
+let map = L.map('sg-map').setView(singapore, 3);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
